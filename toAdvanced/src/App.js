9700556f39a420ea362aab2fb40c50b8c09c1e5d
Tonny Piper <pipertonny@gmail.com>
2022-02-25 22:54:07 +0100

    Custom hooks, PropTypes

--- toAdvanced/src/App.js
+++ toAdvanced/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Setup from './tutorial/8-useContext/setup/1-context-api'
+import Setup from './tutorial/10-prop-types/setup/index'
 //import SetupArray from './tutorial/1-useState/setup/3-useState-array'
 
 function App() {
--- toAdvanced/src/tutorial/10-prop-types/setup/Product.js
+++ toAdvanced/src/tutorial/10-prop-types/setup/Product.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { PropTypes } from 'prop-types';
+import defaultImage from '../../../assets/default-image.jpeg'
 
-const Product = () => {
-  return <article className='product'>single product</article>;
+const Product = ({ image, name, price}) => {
+  return <article className='product'>
+    <img src={image.url} alt = {name } />
+    <h4>{name}</h4>
+    <p>${price}</p>
+  </article>;
 };
+
+Product.prototype = {
+  name: PropTypes.string.isRequired,
+  image: PropTypes.object.isRequired,
+  price: PropTypes.number.isRequired
+}
+
+Product.defaultProps = {
+  name: 'default name',
+  image: defaultImage,
+  price: 3.99
+}
 
 export default Product;
